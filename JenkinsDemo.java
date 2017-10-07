53e53adf4a52822798dbd101335bfd862a77ac0f
HarshadaM <HarshadaM12@gmail.com>
2017-10-07 15:34:04 +0530

    First commit

---
+++ JenkinsDemo.java
@@ -0,0 +1,52 @@
+package com.PD.Jenkins;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+ 
+
+public class JenkinsDemo
+
+{
+
+ 
+
+	@Test
+
+	public void testgooglrsearch()
+	{
+
+ 
+		System.setProperty("webdriver.chrome.driver", "e:\\Pranoday\\Testing\\SeleniumDependencies\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+//it will open the goggle page
+
+		driver.get("http://google.in");
+
+//we expect the title “Google “ should be present
+
+		String Expectedtitle = "Google";
+
+//it will fetch the actual title
+
+		String Actualtitle = driver.getTitle();
+
+		System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+
+//it will compare actual title and expected title
+
+		Assert.assertEquals(Actualtitle, Expectedtitle);
+
+//print out the result
+
+		System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+
+ }
+
+}
